f565163d4cd6c58b8e2de3bb0b29f781adadaad6
metacryst <samrussell99@gmail.com>
2020-05-19 12:03:27 -0500

    preliminary api calls

--- src/App.js
+++ src/App.js
@@ -1,26 +1,171 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import { Route, Link } from "react-router-dom";
+import './App.css'
+
+import Users from "./components/Users.js"
+import Categories from "./components/Categories.js"
+import Items from "./components/Items.js"
+import Needs from "./components/Needs.js"
+import Tiers from "./components/Tiers.js"
+import Links from "./components/Links.js"
+import About from "./components/About.js"
 
 function App() {
+      
+  const [usersData, setUsersData] = useState([]);
+
+  const [apiLink, setApiData] = useState([]);
+  
+  const [error, setError] = useState('')
+
+
+  function getUsersData() {    
+    const url = `http://localhost:8080/api/${apiLink}`
+    console.log(apiLink);
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);        
+        setUsersData(response);
+        console.log(usersData);
+        
+      })
+      .catch(function(error) {
+        setError(error);
+      })
+    }
+  
+  function handleClick() {
+    console.log('click');
+  }
+  
+  function navButtonClick(event) {    
+    setApiData(event.target.getAttribute('name'))
+  }
+    
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='wrapper'>
+      <main>
+        <Route path="/" exact={true} render={() => {
+          return (
+            <>
+              <Link to="/users">
+                <h2 className="users" onClick={navButtonClick} name='user'>users</h2>
+              </Link>
+              <Link to="/tiers">
+                <h2 className="tiers" onClick={navButtonClick} name='tier'>tiers</h2>
+              </Link>
+              <Link to="/categories">
+                <h2 className="categories" onClick={navButtonClick} name='category'>categories</h2>
+              </Link>
+              <Link to="/items">
+                <h2 className="items" onClick={navButtonClick} name='item'>items</h2>
+              </Link>
+              <Link to="/needs">
+                <h2 className="needs" onClick={navButtonClick} name='need'>needs</h2>
+              </Link>
+              <Link to="/links">
+                <h2 className="links" onClick={navButtonClick} name='link'>links</h2>
+              </Link>
+              <Link to="/about">
+                <h2 className="about" >about</h2>
+              </Link>
+            </>
+          )
+        }} />  
+        
+        <Route path="/users" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Users usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+        
+        
+        <Route path="/tiers" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Tiers usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+        
+        <Route path="/categories" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Categories usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+        
+        <Route path="/items" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Items usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+        
+        <Route path="/needs" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Needs usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+        
+        <Route path="/links" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <Links usersData={usersData} getUsersData={getUsersData}/>
+            </>
+          )
+        }}
+        />
+       
+        
+        <Route path="/about" render={() => {
+          return (
+            <>
+              <Link to="/">
+                <h1 className="header">paperclip</h1>
+              </Link>
+              <About />
+            </>
+          )
+        }}
+        />
+        
+      </main>
+  </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+    <p className="aboutText">trade things. maybe you'll get a house</p>
+    <div className="credit">
+      <p>designed by Sam Russell, Brian Guayante, and Trent  2020.</p>
+      <a className="website" href="https://www.samrussell.com/" target="_blank" rel="noopener noreferrer">---> samrussell.com</a>
+    </div>
+    </>
+  )
+}
+
+export default About
